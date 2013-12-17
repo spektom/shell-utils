2bf2eb5e41946e0a87b8d8a8e723c4c7d686b7a8
Michael Spector <spektom@gmail.com>
2013-12-17 16:47:33 +0200

    added job pool

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ shell-utils
 ===========
 
 Various shell utilities
+
+### jp.sh - Job pool ###
+
+This utility allows creating a named job pool limited by specified size,
+and run some commands through it. That means that if a pool size is N,
+at most N commands will be run simultaneously, others will wait until
+a slot will be freed.
+
+#### USAGE ####
+
+```
+USAGE: ./jp.sh <ID> <limit> <command>
+
+Where:
+  <ID>       Job pool identifier
+  <limit>    Job pool size
+  <command>  Command to run
+```
+
---
+++ jp.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+pool_id=$1
+shift
+pool_size=$1
+shift
+if [ -z "$pool_id" ] || [ -z "$pool_size" ] || [ $# -eq 0 ]; then
+	echo
+	echo "USAGE: $0 <ID> <limit> <command>"
+	echo
+	echo "Where:"
+	echo "  <ID>       Job pool identifier"
+	echo "  <limit>    Job pool size"
+	echo "  <command>  Command to run"
+	echo
+	exit 1
+fi
+
+workdir="/tmp/.jp"
+[ -d $workdir ] || mkdir -p $workdir || exit $?
+lock_prefix="$workdir/$pool_id"
+lock_file="$lock_prefix.$$"
+
+lock() {
+	# Critical section:
+	(
+		flock -x 201 || exit 1
+
+		# Wait for other processes to finish
+		num_running=0
+		for l in $(eval ls "${lock_prefix}.*" 2>/dev/null); do
+			if kill -0 $(echo $l | sed 's/.*\.//'); then
+				num_running=$(($num_running+1))
+			else
+				# Remove lock file for non-existent process
+				rm -f $l
+			fi
+		done
+		if [ $num_running -lt $pool_size ]; then
+			touch $lock_file
+			return 0
+		fi
+		return 1
+
+	) 201>$workdir/.lock
+}
+
+unlock() {
+	rm -f $lock_file
+}
+
+trap "unlock; exit 0" INT TERM EXIT
+
+while ! lock; do
+	# Waiting for some process to exit
+	sleep 1
+done
+
+# Run the command
+"$@"
+
